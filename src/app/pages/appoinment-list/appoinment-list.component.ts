68f39e09f9415c3da1196a169152652044bc59f9
Yasinsa Mahanama <yasinsamahanama@gmail.com>
2024-06-12 20:30:06 +0530

    updated

---
+++ src/app/pages/appoinment-list/appoinment-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-appoinment-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './appoinment-list.component.html',
+  styleUrl: './appoinment-list.component.scss'
+})
+export class AppoinmentListComponent {
+
+}
